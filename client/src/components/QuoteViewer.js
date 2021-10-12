34b2b46a4630d53b1db8adebe29364f3f9cbe2ad
fotiou <alexandros@fotiou.me>
2021-10-12 16:38:25 +0300

    Fix quote icons position and bg color

--- client/src/components/QuoteViewer.js
+++ client/src/components/QuoteViewer.js
@@ -3,9 +3,7 @@ import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 
-
 const QuoteViewer = (props) => {
-
 	return (
 		<React.Fragment>
 			<Grid
@@ -13,9 +11,9 @@ const QuoteViewer = (props) => {
 				direction="column"
 				alignItems="center"
 				justifyContent="center"
-			>	
-				<img src="../quotes.svg" className="quote-image left"/>
-				<Box sx={{ width: "100%", maxWidth: 604 }} className={"quote"}>
+			>
+				<Box className={"quote"}>
+					<img src="../quotes.svg" className="quote-image left" />
 					<Typography
 						variant="h3"
 						color="inherit"
@@ -23,8 +21,8 @@ const QuoteViewer = (props) => {
 					>
 						{props.quote}
 					</Typography>
+					<img src="../quotes.svg" className="quote-image right" />
 				</Box>
-				<img src="../quotes.svg"  className="quote-image right"/>
 			</Grid>
 		</React.Fragment>
 	);
